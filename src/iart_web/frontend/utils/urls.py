621f32ccc9303c24878ee08f571cbe4086abaafa
Matthias Springstein <matthias.springstein@gmail.com>
2022-03-02 08:16:52 +0100

    Add a config parser to allow better configuration.

--- src/iart_web/frontend/utils/urls.py
+++ src/iart_web/frontend/utils/urls.py
@@ -4,20 +4,20 @@ from django.conf import settings
 
 
 def media_url_to_image(id):
-    return f'http://localhost:8000{settings.MEDIA_URL}{id[0:2]}/{id[2:4]}/{id}.jpg'
+    return f"{settings.MEDIA_URL}{id[0:2]}/{id[2:4]}/{id}.jpg"
 
 
 def media_url_to_preview(id):
-	return f'http://localhost:8000{settings.MEDIA_URL}{id[0:2]}/{id[2:4]}/{id}_m.jpg'
+    return f"{settings.MEDIA_URL}{id[0:2]}/{id[2:4]}/{id}_m.jpg"
 
 
 def upload_url_to_image(id):
-	return f'http://localhost:8000{settings.UPLOAD_URL}{id[0:2]}/{id[2:4]}/{id}.jpg'
+    return f"{settings.UPLOAD_URL}{id[0:2]}/{id[2:4]}/{id}.jpg"
 
 
 def upload_url_to_preview(id):
-	return f'http://localhost:8000{settings.UPLOAD_URL}{id[0:2]}/{id[2:4]}/{id}_m.jpg'
+    return f"{settings.UPLOAD_URL}{id[0:2]}/{id[2:4]}/{id}_m.jpg"
 
 
 def upload_path_to_image(id):
-    return os.path.join(settings.UPLOAD_ROOT, id[0:2], id[2:4], f'{id}.jpg')
+    return os.path.join(settings.UPLOAD_ROOT, id[0:2], id[2:4], f"{id}.jpg")
