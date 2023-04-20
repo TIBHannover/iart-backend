e109360f6bca3ef92111b3ee10e9618db0821727
Elias Entrup <elias.entrup@tib.eu>
2023-04-20 13:09:20 +0200

    oidc_authentication_backend: Use given and family name

--- src/iart_backend/iart/oidc_authentication_backend.py
+++ src/iart_backend/iart/oidc_authentication_backend.py
@@ -3,6 +3,10 @@ from mozilla_django_oidc.auth import OIDCAuthenticationBackend
 
 class OIDCAB_USERNAME(OIDCAuthenticationBackend):
     def get_username(self, claims):
+        name = claims.get('given_name', '') + ' ' + claims.get('family_name', '')
+        name = name.strip()
+        if name != '':
+            return name
         return claims.get('preferred_username', claims.get('email'))
 
     def create_user(self, claims):
